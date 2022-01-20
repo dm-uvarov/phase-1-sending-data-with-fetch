b092d31e3c08a594665c35bdb26985d12a853820
Jimmy <dm.a.uvarov@gmail.com>
2022-01-20 00:02:27 -0500

    commit tree 9689ee11356b8918a870bb41f2fd3cff9090eb29

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Add your code here
+
+//
+
+
+const sendURL = "http://localhost:3000/users"
+
+function submitData(name_,email_){
+    
+    return fetch(sendURL,{
+        method: "POST", 
+        headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        },
+        body: JSON.stringify({
+            name: name_,
+            email: email_,
+    }),
+    })
+    .then(r => r.json())
+    .then( data => {
+        // let par = document.createElement("p");
+        // par.innerText = data.id
+        // document.querySelector('body').appendChild(par);
+        document.querySelector('body').innerHTML = data.id;
+    })
+    .catch(err=> document.querySelector('body').innerHTML = err.message);
+};
+
+
+
